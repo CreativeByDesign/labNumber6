8884911ac3e679daacc4bccf6e4af48454c898d2
DeVosha <devoshapatterson@DeVoshas-MBP.router>
2019-04-29 04:40:36 -0400

    unfinished lab 6

---
+++ src/labNumber6/labNumber6DiceRoll.java
@@ -0,0 +1,22 @@
+package labNumber6;
+
+import java.util.Scanner;
+
+public class labNumber6DiceRoll {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Please enter the number of sides for your day:");
+		int userNum =  scan.nextInt();
+		scan.nextLine();
+		
+		System.out.println("Would you like to roll again? Y for yes N for now");
+		
+		
+	}
+ public static int generateRandomDieRoll(int Roll) {
+	 
+	 return 0 ;
+ }
+}
+String str
